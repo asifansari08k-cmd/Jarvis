ea97a8242d20c517de7e95a12a10927927e50a4a
asifansari08k-cmd <asif.ansari.08k@gmail.com>
2026-09-13 18:01:34 +0530

    Update Constants.kt

--- app/src/main/java/com/example/jarvis/utils/Constants.kt
+++ app/src/main/java/com/example/jarvis/utils/Constants.kt
@@ -1,2 +1,8 @@
 package com.example.jarvis.utils
-object Constants { const val PREFS_NAME="jarvis_secure_prefs"; const val KEY_API="gemini_api_key"; const val ASSISTANT_NAME="JARVIS" }
+
+object Constants {
+
+    const val PREFS_NAME = "jarvis_preferences"
+
+    const val ASSISTANT_NAME = "JARVIS"
+}
